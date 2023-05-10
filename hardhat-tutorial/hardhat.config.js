c49277bed9319b73b01916c416064dc97420d3ff
shandog <jimshanahandevelops@gmail.com>
2023-05-10 10:59:55 -0700

    changed to sepolia from goerli

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -7,7 +7,7 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    goerli: {
+    sepolia: {
       url: QUICKNODE_HTTP_URL,
       accounts: [PRIVATE_KEY],
     },
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -232,8 +232,8 @@ export default function Home() {
     // If user is not connected to the Goerli network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
     if (chainId !== 5) {
-      window.alert("Change the network to Goerli");
-      throw new Error("Change network to Goerli");
+      window.alert("Change the network to the new Sepolia and not Goerli");
+      throw new Error("Change network to the new Sepolia and not Goerli");
     }
 
     if (needSigner) {
@@ -252,7 +252,7 @@ export default function Home() {
       // Assign the Web3Modal class to the reference object by setting it's `current` value
       // The `current` value is persisted throughout as long as this page is open
       web3ModalRef.current = new Web3Modal({
-        network: "goerli",
+        network: "sepolia",
         providerOptions: {},
         disableInjectedProvider: false,
       });
