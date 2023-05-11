ae08e9652e78321556297ad2d223c0cb41e4ba1e
shandog <jimshanahandevelops@gmail.com>
2023-05-11 13:22:20 -0700

    updated url

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x1e5D1EE685a76d94b2a2faeC988103fF8b5D19F4";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-olive-six.vercel.app/";
+const METADATA_URL = "https://nft-collection-olive-six.vercel.app/api";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
